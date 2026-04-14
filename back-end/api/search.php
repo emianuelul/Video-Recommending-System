8773ad5a0e80ad857a1b581c4da4966d3a9f0349
emianuelul <emanuel.marin0000@gmail.com>
2026-04-14 18:51:54 +0300

    add basic search + basic filters + basic html

--- back-end/api/search.php
+++ back-end/api/search.php
@@ -1,9 +1,43 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 require_once '../config.php';
 
-$query = urlencode("teatrul national iasi");
-$url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" . $query . "&key=" . YT_API_KEY;
+// videoDuration
+/*
+ * any
+ * long -> longer than 20mins
+ * medium -> 4mins - 20mins
+ * short -> <4mins
+ */
+
+// publishedAfter / publishedBefore
+// RFC 3339 formatted date-time value (ex. : 1970-01-01T00:00:00Z)
+
+// relevanceLanguage
+// ISO 639-1 two-letter language code (ex. : ro 4 romanian)
+
+// order
+/*
+ * date
+ * rating
+ * relevance
+ * title
+ * videoCount
+ * viewCount
+ */
+
+$q = "q=" . urlencode($_GET['q']);
+$videoDuration = isset($_GET['videoDuration']) ? "&videoDuration=" . $_GET['videoDuration'] : '';
+$publishedAfter = isset($_GET['publishedAfter']) ? "&publishedAfter=" . $_GET['publishedAfter'] : '';
+$publishedBefore = isset($_GET['publishedBefore']) ? "&publishedBefore=" . $_GET['publishedBefore'] : '';
+$relevanceLanguage = isset($_GET['relevanceLanguage']) ? "&relevanceLanguage=" . $_GET['relevanceLanguage'] : '';
+$order = isset($_GET['order']) ? "&order=" . $_GET['order'] : '';
+$resultNumber = "&maxResults=" . 20;
+
+$params = $q . $videoDuration . $publishedAfter . $publishedBefore . $relevanceLanguage . $order . $resultNumber;
+
+$url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&" . $params . "&key=" . YT_API_KEY;
+
 $req = file_get_contents($url);
 
-header("Content-Type: application/json");
 echo $req;
