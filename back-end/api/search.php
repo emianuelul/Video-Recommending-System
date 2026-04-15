d3f6dfe8206f4bd3f5ecc8263a723774e2031367
emianuelul <emanuel.marin0000@gmail.com>
2026-04-15 20:35:41 +0300

    add second query to search to get more info about each video

--- back-end/api/search.php
+++ back-end/api/search.php
@@ -2,30 +2,6 @@
 header('Access-Control-Allow-Origin: *');
 require_once '../config.php';
 
-// videoDuration
-/*
- * any
- * long -> longer than 20mins
- * medium -> 4mins - 20mins
- * short -> <4mins
- */
-
-// publishedAfter / publishedBefore
-// RFC 3339 formatted date-time value (ex. : 1970-01-01T00:00:00Z)
-
-// relevanceLanguage
-// ISO 639-1 two-letter language code (ex. : ro 4 romanian)
-
-// order
-/*
- * date
- * rating
- * relevance
- * title
- * videoCount
- * viewCount
- */
-
 $q = "q=" . urlencode($_GET['q']);
 $videoDuration = isset($_GET['videoDuration']) ? "&videoDuration=" . $_GET['videoDuration'] : '';
 $publishedAfter = isset($_GET['publishedAfter']) ? "&publishedAfter=" . $_GET['publishedAfter'] : '';
@@ -33,11 +9,28 @@ $publishedBefore = isset($_GET['publishedBefore']) ? "&publishedBefore=" . $_GET
 $relevanceLanguage = isset($_GET['relevanceLanguage']) ? "&relevanceLanguage=" . $_GET['relevanceLanguage'] : '';
 $order = isset($_GET['order']) ? "&order=" . $_GET['order'] : '';
 $resultNumber = "&maxResults=" . 20;
+$apiKey = "&key=" . YT_API_KEY;
 
 $params = $q . $videoDuration . $publishedAfter . $publishedBefore . $relevanceLanguage . $order . $resultNumber;
 
-$url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&" . $params . "&key=" . YT_API_KEY;
+$url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&" . $params . $apiKey;
 
 $req = file_get_contents($url);
+$data = json_decode($req, true);
 
-echo $req;
+$idsArray = [];
+if (isset($data['items'])) {
+    foreach ($data['items'] as $item) {
+        if (isset($item['id']['videoId'])) {
+            $idsArray[] = $item['id']['videoId'];
+        }
+    }
+}
+
+$videoIds = implode(',', $idsArray);
+
+$detailsUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics,topicDetails&id=" . $videoIds . $apiKey;
+$detailsReq = file_get_contents($detailsUrl);
+
+header("Content-Type: application/json");
+echo $detailsReq;
