ddcd33b84c30f88fafd510e90f8b5decaae68e90
emianuelul <emanuel.marin0000@gmail.com>
2026-04-22 22:06:41 +0300

    login logic + implement simple token authentication system

--- back-end/api/login.php
+++ back-end/api/login.php
@@ -1,23 +1,36 @@
 <?php
-session_start();
 require '../../db/database.php';
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = trim($_POST['username'] ?? '');
-    $password = $_POST['password'] ?? '';
+    $data = json_decode(file_get_contents('php://input'), true);
+    $username = trim($data["username"] ?? ' ');
+    $password = trim($data["password"] ?? ' ');
 
     if ($username === '' || $password === '') {
         die('Username and password are required.');
     }
 
-    $stmt = $db->prepare("SELECT id, username, password FROM users WHERE username = :username");
+    $stmt = $db->prepare("SELECT id, username, password_hash FROM users WHERE username = :username");
     $stmt->execute([':username' => $username]);
     $user = $stmt->fetch();
 
-    if ($user && password_verify($password, $user['password'])) {
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
+    if ($user && password_verify($password, $user['password_hash'])) {
+        $token = bin2hex(random_bytes(64));
+
+        $stmt = $db->prepare("INSERT INTO user_tokens (user_id, token) VALUES (:userId, :token)");
+        $stmt->execute([
+            ':userId' => $user['id'],
+            ':token' => $token
+        ]);
+
+        http_response_code(200);
+        header('Content-Type: application/json');
+        echo json_encode(["status" => 200, "message" => "Logged In Successfully", "token" => $token, "userId" => $user['id']]);
     } else {
-        echo "Invalid username or password.";
+
+        http_response_code(500);
+        echo json_encode(["status" => 500, "message" => "Failed to log in"]);
     }
 }
---
+++ front-end/login-page/login-script.js
@@ -0,0 +1,44 @@
+const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
+const submitBtn = document.querySelector('#login-submit');
+
+const url = "http://localhost:8081/api/login.php";
+
+submitBtn.addEventListener('click', () => {
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    }).then(async r => {
+        const resp = await r.json();
+        switch (resp.status) {
+            case 200: {
+                alert(resp.message);
+                
+                localStorage.setItem("userId", resp.userId);
+                localStorage.setItem("token", resp.token);
+                
+                // console.log(localStorage);
+                // maybe go to home page or smth
+                
+                break;
+            }
+            case 409: {
+                alert(resp.message);
+                break;
+            }
+            default: {
+                alert("Unexpected error: " + resp.message);
+                break;
+            }
+        }
+    })
+})
