3f4d84d97e7251b188e5782b60a84af41bb07a0c
emianuelul <emanuel.marin0000@gmail.com>
2026-04-30 12:13:11 +0300

    add allowed origins array to rounter, to permit 127.0.0.1:8001

--- back-end/router.php
+++ back-end/router.php
@@ -1,8 +1,13 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:8001');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Allow-Credentials: true');
+
+$allowed_origins = ["http://127.0.0.1:8001", "http://localhost:8001"];
+
+if (in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins, true)) {
+    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
